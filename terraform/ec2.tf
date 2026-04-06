f2f158bb617cdb3f3836513241df679b0968a9d1
Deepak Ramakrishna Panada <de40133869@wipro.com>
2026-04-06 13:01:29 +0530

    terraform workflow created

---
+++ terraform/ec2.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "web_sg" {
+
+  name        = "company-website-sg"
+  description = "Allow web and ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30007
+    to_port     = 30007
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "company_server" {
+
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t3.micro"
+
+  key_name = "aws-dev-key"
+
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+   # Root volume configuration
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
+
+  tags = {
+    Name = "company-website-server"
+  }
+
+}
---
+++ terraform/output.tf
@@ -0,0 +1,3 @@
+output "public_ip" {
+    value = aws_instance.company_server.public_ip 
+}
